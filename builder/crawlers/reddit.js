431e740b1331b9f753fa77bb1851211f37b6cfc1
Alexandru Branza <branza.alex@gmail.com>
2023-12-19 18:59:07 +0200

    Fix Reddit Crawler / Automate Retrieving Reddit Followers

--- builder/crawlers/reddit.js
+++ builder/crawlers/reddit.js
@@ -1,22 +1,44 @@
 const needle = require('needle')
+const qs = require('querystring')
 
-const redditUser = 'jaruba_dev'
+async function getRedditDetails() {
+    const clientId = process.env.REDDIT_CLIENT_ID
+    const clientSecret = process.env.REDDIT_CLIENT_SECRET
 
-async function getKarma() {
-	const resp = await needle('get', `https://www.reddit.com/user/${redditUser}/about.json`, { json: true })
-	if (resp.statusCode !== 200 || !resp.body?.data?.total_karma)
-		throw Error(`Could not retrieve reddit karma, http status code ${resp.statusCode}`)
-	return resp.body.data.total_karma
+    const redditUser = 'jaruba_dev'
+    const redditPass = process.env.REDDIT_PASS
+
+    const authString = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
+    
+    const resp = await needle('post', 'https://www.reddit.com/api/v1/access_token', qs.stringify({
+        grant_type: 'password',
+        username: redditUser,
+        password: redditPass
+    }), {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${authString}`
+        }
+    })
+
+    if (resp.statusCode !== 200 || !resp?.body?.access_token)
+        throw Error(`Could not retrieve Reddit access token, http status code ${resp.statusCode}`)
+
+    const bearerToken = resp.body.access_token
+
+    const me = await needle('get', 'https://oauth.reddit.com/api/v1/me', null, {
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        }
+    })
+
+    if (me.statusCode !== 200 || !me?.body?.total_karma || !me?.body?.subreddit?.subscribers)
+        throw Error(`Could not retrieve Reddit karma / followers, http status code ${resp.statusCode}`)
+
+    return {
+        karma: me.body.total_karma.toLocaleString(),
+        followers: me.body.subreddit.subscribers.toLocaleString(),
+    }
 }
 
-module.exports = async function() {
-	const redditKarma = await getKarma()
-	return {
-		karma: redditKarma.toLocaleString(),
-
-		// reddit followers cannot be seen publicly
-		// and the followers count cannot be retrieved
-		// from the API either, hardcoding it for now
-		followers: '152',
-	}
-}
+module.exports = getRedditDetails()
